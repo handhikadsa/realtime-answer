fab90e35c6bb178c8673c1c3706a7b9794fda97a
Dhika <59341485+handhikadsa@users.noreply.github.com>
2024-05-11 22:27:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,39 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import { set, useForm } from "react-hook-form"
 import { useEffect, useState } from 'react';
+import { collection, addDoc } from "firebase/firestore";
+import { db } from './utils/firebase';
 
 function App() {
-  const [newData, setNewData] = useState([])
-  const { register, handleSubmit } = useForm()
+  const [count, setCount] = useState(0)
+  const { register, handleSubmit, reset } = useForm()
+  const [isAnswered, setIsAnswered] = useState(false)
   
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log(data)
-    setNewData([...newData, data])
+    try {
+      const docRef = await addDoc(collection(db, "answers"), {
+        answer: data.answer,    
+      });
+      setIsAnswered(true)
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    reset();
   }
 
-  useEffect(() => {
-    console.log(newData)
-  }, [newData])
-    
   return (
     <div className="App">
       <header className="App-header">
-        {
-          newData.map((item, index) => {
-            return (
-              <div key={index} className="text-danger" style={{fontSize: 16}} role="alert">
-                {item.answer}
-              </div>
-            )
-          })
-        }
-        <form className="w-75" onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-3">
-            <label htmlFor="answer" className="form-label" style={{fontSize: 30}}>Email address</label>
-            <input {...register("answer")} type="text" className="form-control" id="answer" aria-describedby="answer" />
+        <div className="position-absolute top-0 start-0 mt-5">
+          <div className="row justify-content-center">
+            <div className="col-4">
+              <img className="w-100" src="/youth-logo.png" alt="" />
+            </div>
           </div>
-          <button type="submit" className="btn btn-primary px-5">Submit</button>
-        </form>
+        </div>
+        {
+          isAnswered ?
+          <div className="alert alert-success" role="alert">
+            It's okay! Roh Kudus bersamamu!
+          </div> :  
+            <form className="w-75" onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-3">
+                <label htmlFor="answer" className="form-label mb-3" style={{fontSize: 25}}>Apa yang menjadi tantangan terbesar buat kamu dalam menjalankan ketaatan?</label>
+                <input {...register("answer")} type="text" className="form-control mb-3" id="answer" aria-describedby="answer" placeholder="tulis jawaban mu di sini!" />
+              </div>
+              <button type="submit" className="btn btn-primary px-5">Kirim</button>
+            </form>
+          }
       </header>
     </div>
   );
--- src/Result.js
+++ src/Result.js
@@ -1,14 +1,65 @@
 import './App.css';
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { db } from './utils/firebase';
+import { useEffect, useState, useRef, useCallback } from 'react';
 
-const Results = ({ results }) => { 
+const Answer = ({ item }) => {
+    const [position, setPosition] = useState({ top: 0, left: 0 });
+    const ref = useRef();
+  
+    const setPositionCallback = useCallback(() => {
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        setPosition({
+          top: `${Math.random() * (100 - rect.height / window.innerHeight * 100)}vh`,
+          left: `${Math.random() * (100 - rect.width / window.innerWidth * 100)}vw`,
+        });
+      }
+    }, []);
+  
+    useEffect(() => {
+      setPositionCallback();
+    }, [setPositionCallback]);
+  
     return (
-        <div>
-            <h1>Results</h1>
-            <ul>
-                {results.map((result, index) => (
-                    <li key={index}>{result}</li>
-                ))}
-            </ul>
+      <div 
+        ref={ref}
+        className="text-white bg-dark p-3 rounded-3 shadow-lg"
+        style={{
+          animationName: 'flyAround',
+          animationDuration: `${Math.random() * 10 + 10}s`,
+          animationDelay: `${Math.random() * 5}s`,
+          animationIterationCount: 'infinite',
+          position: 'absolute',
+          top: position.top,
+          left: position.left,
+          transition: 'all 0.5s ease',
+          fontSize: '20px',
+        }}
+      >
+        {item.answer}
+      </div>
+    );
+};  
+
+const Results = () => { 
+    const [answer, setAnswer] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "answers"), (snapshot) => {
+      const newData = snapshot.docs.map((doc) => ({...doc.data(), id:doc.id }));
+      setAnswer(newData);
+    });
+
+    // Clean up the subscription on unmount
+    return () => unsubscribe();
+  }, []); // Only set up the subscription when the component mounts
+
+    return (
+        <div className="App">
+            <header style={{backgroundColor: "#282c34", height: "100vh"}}>
+                {answer?.map((item, i) => <Answer key={i} item={item} />)}
+            </header>
         </div>
     )
 }
