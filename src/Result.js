53ef2a7e75a4036a9f555ec03e4596869dc2a780
Dhika <59341485+handhikadsa@users.noreply.github.com>
2024-05-12 08:55:40 +0700

    update calculation answer bubble

--- src/Result.js
+++ src/Result.js
@@ -5,14 +5,22 @@ import { useEffect, useState, useRef, useCallback, useLayoutEffect } from 'react
 
 const Answer = ({ item }) => {
     const [position, setPosition] = useState({ top: 0, left: 0 });
+    const [step, setStep] = useState(0);
+    const [maxSteps, setMaxSteps] = useState(Math.floor(Math.random() * 5) + 5); // Random number between 5 and 10
     const ref = useRef();
   
     const setPositionCallback = useCallback(() => {
       if (ref.current) {
-        const rect = ref.current.getBoundingClientRect();
+        const rectWidth = ref.current.offsetWidth;
+        const rectHeight = ref.current.offsetHeight;
+        const cellSize = 100; // Size of each cell in the grid
+        const gridRows = Math.floor((window.innerHeight - rectHeight - 200) / cellSize);
+        const gridColumns = Math.floor((window.innerWidth - rectWidth - 200) / cellSize);
+        const randomRow = Math.floor(Math.random() * gridRows);
+        const randomColumn = Math.floor(Math.random() * gridColumns);
         setPosition({
-          top: `${Math.random() * (window.innerHeight - rect.height)}px`,
-          left: `${Math.random() * (window.innerWidth - rect.width)}px`,
+          top: `${randomRow * cellSize + 100}px`,
+          left: `${randomColumn * cellSize + 100}px`,
         });
       }
     }, []);
@@ -27,7 +35,7 @@ const Answer = ({ item }) => {
             className="text-dark bg-light p-3 rounded-3 shadow-lg fw-bold text-container"
             style={{
                 animationName: 'flyAround',
-                animationDuration: `${Math.random() * 10 + 10}s`,
+                animationDuration: `${Math.random() * 8 + 8}s`,
                 animationDelay: `${Math.random() * 5}s`,
                 animationIterationCount: 'infinite',
                 position: 'absolute',
